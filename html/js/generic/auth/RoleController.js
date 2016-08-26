0e8f0b6a50ba7cf67c78b31aa5e8da0166637195
Mel Liu <privatemiao@gmail.com>
2016-08-26 15:52:29 +0800

    aa

--- html/js/generic/auth/RoleController.js
+++ html/js/generic/auth/RoleController.js
@@ -1,6 +1,7 @@
 materialAdmin.controller('RoleController', function($scope, UserService) {
 	UserService.getAllMenu().then(function(response) {
 		$scope.nodes = response;
+		console.log($scope.nodes);
 	}, function(response) {
 		console.log("Error", response);
 	});
